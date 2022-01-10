dae9e52060cb69366d9417d8bd5e8be56e171253
RMuthuPrakash <prakashmuthu216@gmail.com>
2022-01-10 19:42:23 +0530

    add code to repository

---
+++ org/comp/Desktop.java
@@ -0,0 +1,16 @@
+package org.comp;
+
+public class Desktop extends Computer {
+	private void desktopSize() {
+		System.out.println("my desktopSize is:4Gb");
+		}
+	public static void main(String[] args) {
+		Desktop d =new Desktop();
+		d.desktopSize();
+		d.computerModel();
+	}
+	
+
+	}
+
+
